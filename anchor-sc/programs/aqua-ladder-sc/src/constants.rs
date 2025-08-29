3dd332f51c7c2e90b04ff979b8b7e0fc3b9cd92b
darwindowndev <darwindowndev@proton.me>
2025-08-29 18:24:48 +0200

    fix: adjusted constants. removed unused ones

--- anchor-sc/programs/aqua-ladder-sc/src/constants.rs
+++ anchor-sc/programs/aqua-ladder-sc/src/constants.rs
@@ -2,9 +2,6 @@ use anchor_lang::{prelude::*, solana_program::native_token::LAMPORTS_PER_SOL};
 
 pub const DISCRIMINATOR: usize = 8;
 
-// ladder constants
-pub const LADDER_INITIAL_CAP: u64 = 2222 * LAMPORTS_PER_SOL;
-
 // participation constraints
-pub const MINIMUM_PARTICIPATION_SOL: u64 = LAMPORTS_PER_SOL / 10; // 0.1 SOL
-pub const MAXIMUM_PARTICIPATION_SOL: u64 = 20 * LAMPORTS_PER_SOL; // 20 SOL
+pub const MINIMUM_PARTICIPATION_SOL: u64 = LAMPORTS_PER_SOL; // 1 SOL
+pub const MAXIMUM_PARTICIPATION_SOL: u64 = 30 * LAMPORTS_PER_SOL; // 30 SOL
